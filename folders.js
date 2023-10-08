167dc51f5c5edfd71833d4a35d40692e6e5c07d2
fantaapple <143857696+fantaapple@users.noreply.github.com>
2023-10-08 13:34:11 -0400

    Update folders.js

--- folders.js
+++ folders.js
@@ -1,1 +1,22 @@
+function showFolders() {
+    //variable folderNavHTML represents the new navigation bar to look at folders
+    const folderNavHTML = `
+        <div class="folderNav" style="background-color: white; width: 100%;">
+            <button id="folder1" style="background-color: #add8e6;">Folder 1</button>
+        </div>
+    `;
+    //variable showNotesTableHTML represents the new table to show any notes made
+    const showNotesTableHTML = `
+        <table class="showNotes">
+            <tr>
+                <td>Nothing here! Add a note!</td>
+            </tr>
+            <tr>
+                <td><button id="createNote">Create Note!</button></td>
+            </tr>
+        </table>
+    `;
+    //add the two variables to the body of popup.html
+    document.body.innerHTML += folderNavHTML + showNotesTableHTML;
+}
 
