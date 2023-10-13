4712eed7e0ed58f049bc7dd60ab4dab3dd5ebed2
fantaapple <143857696+fantaapple@users.noreply.github.com>
2023-10-13 11:20:16 -0400

    Update accountManagement.js

--- accountManagement.js
+++ accountManagement.js
@@ -53,10 +53,13 @@ function tryLogin() {
     //the email and password input boxes were NOT empty
     if (email !== "" && password !== "") {
        
+        
+
         //get the loginMenu table
         const loginMenu = document.querySelector(".loginMenu"); 
         //remove the loginMenu from function login() above
         loginMenu.parentNode.removeChild(loginMenu);
+
         //variable script = a new <script> element
         const script = document.createElement('script');
         //the script source is folders.js
@@ -67,6 +70,8 @@ function tryLogin() {
         script.onload = function() {
             showFolders();
         } 
+        
+        
     }
     //otherwise, if the email and password IS empty
     else {
@@ -76,6 +81,8 @@ function tryLogin() {
 }
 
 function register() {
+    
+
     //get the accountMenu from the popup
     const accountMenu = document.querySelector(".accountMenu");
     //varibale registerMenu = new <table> with class registerMenu
@@ -112,6 +119,55 @@ function register() {
     registerMenu.style.justifyContent = "center";
     registerMenu.style.alignItems = "center";
     registerMenu.style.height = "100vh";
+
+
+    tryRegister.addEventListener('click', function(){
+        const emailInput = document.getElementById('email');
+        const passwordInput = document.getElementById('password');
+        const nameInput = document.getElementById('userName');
+        const passConfirm = document.getElementById('confirmedPassword');
+    
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
+        const name = nameInput.value;
+        const confirmedPassword = passConfirm.value.trim();
+
+
+
+        if (password.length < 8) {
+            alert("Password must be at least 8 characters long.");
+        } 
+        else if (password.length > 12) {
+            alert("Password is too long. Maximum length is 12 characters.");
+        }
+        else if (!email.includes('@'))
+        {
+            alert("Please provide a valid email address.");
+        }
+        else if (password !== confirmedPassword) {
+            alert("Passwords do not match.");
+        }
+        else if (name.includes(' ') || name.length === 0 || name.length > 20) {
+            alert("Please enter a correct name.");
+        }
+        else{
+            //get the loginMenu table
+            const registerMenu = document.querySelector(".registerMenu"); 
+            //remove the loginMenu from function login() above
+            registerMenu.parentNode.removeChild(registerMenu);
+
+            //variable script = a new <script> element
+            const script = document.createElement('script');
+            //the script source is folders.js
+            script.src = 'folders.js';
+            //add folders.js to the document
+            document.head.appendChild(script);
+            //when folders.js is loaded, call showFolders()
+            script.onload = function() {
+                showFolders();
+            }
+        }
+    });
 }
 
 
