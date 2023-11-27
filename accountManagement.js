6ec02d61894e909e67b4fcda9a274b1533636ec0
Christian King <143857696+ChristianKing23@users.noreply.github.com>
2023-11-27 15:31:55 -0500

    Updated accountManagement.js: now with firebase implementations!
    This handles logging in and registering for Notelee.
    
    - adds event listeners to the login and register buttons on popup.html
    
    register():
    - adds a register menu with an email field, password field, and confirm password field. 
    - if the fields look good, create an authenticated user in firebase using their email and password.
    - create the user in the realtime database with a unique user id, email, password, and four empty folders.
    - set their data in chrome storage: their email, userid, and an empty status.
    - send an email verification link to the user's email. they will become verified by clicking the link in the email. reload the plugin to the beginning.
    
    tryLogin():
    - gets email and password from login menu.
    - if they aren't empty, search for an authenticated user with that email and password. 
    - if found with a verified email, set their status as active in chrome storage, then load folder 1.
    - if found and they are not verified, then alert them. they cannot continue.
    
    replaceAccountMenuWithLogin() and replaceAccountMenuwithRegister(): replaced the menu in popup.html with a login or register menu.
    
    addManageAccountButton(): adds a icon button to the top right next to the question mark for managing the user's account.
    
    addFolderNav(): adds folder navigation bar

--- accountManagement.js
+++ accountManagement.js
@@ -1,10 +1,17 @@
+import { app } from './firebase-main.js';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification } from "/firebase-auth.js";
+import { getDatabase, ref, set, get } from "/firebase-database.js";
+import { loadFolderEventListener } from "/folders.js";
+
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+
 /*
-1. Get the menu table from popup.html. Delete it.
-2. Create a login menu using the table element. Add it into the document, replacing the first menu.
-3. Add css properties to the login menu.
-4. Add an event listener to the login button that was just created. When clicked, call function tryLogin().
+Purpose: replace html with a login menu or register menu
 */
-function login() {
+function replaceAccountMenuWithLogin() {
+    console.log("On function replaceAccountMenuWithLogin().");
     //1. Get the menu table from popup.html.
     const accountMenu = document.querySelector(".accountMenu");
     //2. Create a login menu using the table element.
@@ -23,10 +30,12 @@ function login() {
         </tr>
         <tr>
             <td><button id="tryLogin">Login</td>
+            <td><button id="forgotPassword">Forgot Password?</td>
         </tr>
     `;
     //2. the accountMenu table in popup.html is being replaced with the loginMenu table above.
     accountMenu.parentNode.replaceChild(loginMenu, accountMenu);
+    console.log("Replaced accountMenu with loginMenu.");
     //3. the loginMenu table css properties
     loginMenu.style.display = "flex";
     loginMenu.style.justifyContent = "center";
@@ -35,49 +44,151 @@ function login() {
     //4. Get the login button.
     const tryLoginButton = document.getElementById('tryLogin');
     //4. Add event listener to login button. call tryLogin().
-    tryLoginButton.addEventListener('click', tryLogin); 
+    tryLoginButton.addEventListener('click', tryLogin);
+}
+function replaceAccountMenuwithRegister() {
+    //1. get the popup menu
+    const accountMenu = document.querySelector(".accountMenu");
+    //1. Create a new table to serve as the menu for registering users.
+    const registerMenu = document.createElement("table");
+    //1. add a class to the register menu.
+    registerMenu.classList.add("registerMenu");
+    //1. registerMenu HTML code
+    registerMenu.innerHTML = `
+    <tr>
+        <td>EMAIL:</td>
+        <td><input type="text" id="email"></td>
+    </tr>
+    <tr>
+        <td>PASSWORD:</td>
+        <td><input type="password" id="password"></td>
+    </tr>
+    <tr>
+        <td>CONFIRM PASSWORD:</td>
+        <td><input type="password" id="confirmedPassword"></td>
+    </tr>
+    <tr>
+        <td><button id="tryRegister">Register</td>
+    </tr>
+`;
+    //1. replace the popup accountMenu with the registerMenu
+    accountMenu.parentNode.replaceChild(registerMenu, accountMenu);
+    console.log("Added register menu.");
+    //style the registerMenu
+    registerMenu.style.display = "flex";
+    registerMenu.style.justifyContent = "center";
+    registerMenu.style.alignItems = "center";
+    registerMenu.style.height = "100vh";
 }
 
-//#########################################################################################################################################################
-//#########################################################################################################################################################
-//#########################################################################################################################################################
-//#########################################################################################################################################################
 
+//#########################################################################################################################################################
+//#########################################################################################################################################################
+//#########################################################################################################################################################
+//#########################################################################################################################################################
 /*
-1. Retrive the text inputted into the email and password input boxes. Trim them if needed.
-2. If they are NOT empty, remove the login menu. Call buttonClick("login") to load folder1.
-3. If they ARE empty, alert that they need to enter something. 
+Purpose: find the user in the database, if they are there then add them to chrome storage with status "active"
+if they aren't there then send an error
 */
 function tryLogin() {
+
     //1. get the email and password from the input boxes
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
+
     //1. trim them
     const email = emailInput.value.trim();
     const password = passwordInput.value.trim();
+
+
     //2. the email and password input boxes were NOT empty
     if (email !== "" && password !== "") {
-        //get the loginMenu table
-        const loginMenu = document.querySelector(".loginMenu"); 
-        //remove the loginMenu from function login() above
-        loginMenu.parentNode.removeChild(loginMenu);
-        //variable script = a new <script> element
-        const script = document.createElement('script');
-        //the script source is folders.js
-        script.src = 'folders.js';
-        //add folders.js to the document
-        document.head.appendChild(script);
-        //when folders.js is loaded, call showFolders()
-        script.onload = function() {
-            buttonClick("login");
-        }  
+        console.log("Trying login with email " + email + " and password " + password + ".");
+
+        //use email and password to authenticate the user
+        //if successful then add user to the database
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+
+                //define user, userID
+                const user = userCredential.user;
+                const userId = user.uid;
+
+                //get a referene for the user in the database using userid
+                const userRef = ref(database, `users/${userId}`);
+                get(userRef)
+
+                    .then((snapshot) => {
+
+                        //if the user is in the database
+                        if (snapshot.exists()) {
+
+                            console.log("User is in the system.");
+
+                            //get the userdata from the snapshot to the database
+                            const userData = snapshot.val();
+
+                            if (user.emailVerified) {
+
+                                const chromeUserData = {
+                                    email: userData.email,
+                                    userid: userData.userid,
+                                    status: "active"
+                                };
+    
+                                //in chrome storage, set the userdata
+                                chrome.storage.sync.set({ userData: chromeUserData });
+    
+                                //remove the loginMenu table
+                                const loginMenu = document.querySelector(".loginMenu");
+                                loginMenu.remove();
+    
+                                //add folder navigation and loading notes text
+                                addFolderNav();
+    
+                                //add ManageAccount button to the top bar
+                                addManageAccountButton();
+
+
+                                loadFolderEventListener("login", userId);
+                            } else {
+                                alert("Please verify your email before continuing with notelee.");
+                            }
+
+
+
+                            //chromeuserdata is the data being added to chrome storage
+                            //make the status active since the user is currently using the plugin
+                           
+                        }
+                        else {
+
+                            //if user is not in the database
+                            console.log("User not in database");
+                        }
+                    })
+
+                    //if any error from firebase
+                    .catch((error) => {
+                        alert("Error getting user data from Firebase: ", error);
+                    })
+
+            })
+
+            //if authentication fails
+            .catch((error) => {
+                alert("Authentication failed. " + error.message);
+            });
     }
-    //3. otherwise, if the email and password IS empty
+
+    //if user left the input fields blank
     else {
         //send an alert to let them know
         alert("Please provide both email and password.");
     }
 }
+
+
 
 //#########################################################################################################################################################
 //#########################################################################################################################################################
@@ -92,94 +203,152 @@ function tryLogin() {
 */
 
 function register() {
-    //1. get the popup menu
-    const accountMenu = document.querySelector(".accountMenu");
-    //1. Create a new table to serve as the menu for registering users.
-    const registerMenu = document.createElement("table");
-    //1. add a class to the register menu.
-    registerMenu.classList.add("registerMenu");
-    //1. registerMenu HTML code
-    registerMenu.innerHTML = `
-        <tr>
-            <td>EMAIL:</td>
-            <td><input type="text" id="email"></td>
-        </tr>
-        <tr>
-            <td>NAME:</td>
-            <td><input type="text" id="userName"></td>
-        </tr>
-        <tr>
-            <td>PASSWORD:</td>
-            <td><input type="password" id="password"></td>
-        </tr>
-        <tr>
-            <td>CONFIRM PASSWORD:</td>
-            <td><input type="password" id="confirmedPassword"></td>
-        </tr>
-        <tr>
-            <td><button id="tryRegister">Register</td>
-        </tr>
-    `;
-   //1. replace the popup accountMenu with the registerMenu
-    accountMenu.parentNode.replaceChild(registerMenu, accountMenu);
-   //style the registerMenu
-    registerMenu.style.display = "flex";
-    registerMenu.style.justifyContent = "center";
-    registerMenu.style.alignItems = "center";
-    registerMenu.style.height = "100vh";
-    //2. add event listener to the tryRegister button when clicked. 
-    tryRegister.addEventListener('click', function(){
-        //3. Retrive the texts entered in all 4 input boxes.
+    console.log("On register().");
+
+    //replace html
+    replaceAccountMenuwithRegister();
+
+    //add event listener to tryRegister
+    const tryRegister = document.getElementById('tryRegister');
+    tryRegister.addEventListener('click', function () {
+        console.log("On tryRegister().");
+
+        // Retrive the texts entered in all 3 input boxes.
         const emailInput = document.getElementById('email');
         const passwordInput = document.getElementById('password');
-        const nameInput = document.getElementById('userName');
         const passConfirm = document.getElementById('confirmedPassword');
         const email = emailInput.value.trim();
         const password = passwordInput.value.trim();
-        const name = nameInput.value;
         const confirmedPassword = passConfirm.value.trim();
-        //3. Create rules for what can and cannot be entered.
-        if (password.length < 8) {
-            alert("Password must be at least 8 characters long.");
-        } 
-        else if (password.length > 12) {
-            alert("Password is too long. Maximum length is 12 characters.");
+
+
+        //Create rules for what can and cannot be entered.
+        if (password.length < 6) {
+            alert("Password must be at least 6 characters long.");
         }
-        else if (!email.includes('@'))
-        {
+        else if (!email.includes('@')) {
             alert("Please provide a valid email address.");
         }
         else if (password !== confirmedPassword) {
             alert("Passwords do not match.");
         }
-        else if (name.includes(' ') || name.length === 0 || name.length > 20) {
-            alert("Please enter a correct name.");
-        }
-        //4. if the input boxes pass all the rules, remove the register menu.
-        //Then, call buttonClick("login") to load folder1.
-        else{
-            //get the register menu table
-            const registerMenu = document.querySelector(".registerMenu"); 
-            //remove it
-            registerMenu.parentNode.removeChild(registerMenu);
-            //variable script = a new <script> element
-            const script = document.createElement('script');
-            //the script source is folders.js
-            script.src = 'folders.js';
-            //add folders.js to the document
-            document.head.appendChild(script);
-            //when folders.js is loaded, call buttonClick("login")
-            script.onload = function() {
-                buttonClick("login");
-            }
+
+        //if all fields are correct then continue
+        else {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+
+                    const user = userCredential.user;
+
+
+                    console.log("Trying to reigster with " + email + ", " + password);
+
+                    //create the user in the database using the email and password
+
+
+                    const userData = {
+                        userid: user.uid,
+                        email: user.email,
+                        password: password,
+                        folder1: [],
+                        folder2: [],
+                        folder3: [],
+                        folder4: []
+                    };
+
+                    //use chrome storage to save the user with an active status
+                    const chromeUserData = {
+                        email: email,
+                        userid: user.uid,
+                        status: ""
+                    };
+                    chrome.storage.sync.set({ userData: chromeUserData });
+
+                    //set the user's data 
+                    const userRef = ref(database, `users/${user.uid}`);
+
+                    //if it works..
+                    set(userRef, userData).then(() => {
+                        alert("User data added to database.");
+                        location.reload();
+                    }).catch((error) => {
+                        //if it doesnt work
+                        console.error("Error adding user data: ", error);
+                    });
+
+
+                    sendEmailVerification(user)
+                        .then(() => {
+                            console.log("accountManagement.js: email verification sent to " + email + "...");
+                            alert("Email verification sent! Please check your email to verify your account.");
+
+                        })
+                        .catch((error) => {
+                            alert(error);
+                        });
+
+
+
+
+                })
+                .catch((error) => {
+                    alert("Registration failed: " + error.message);
+                });
+
         }
     });
 }
+//#########################################################################################################################################################
+//#########################################################################################################################################################
+//#########################################################################################################################################################
+//#########################################################################################################################################################
+export function addFolderNav() {
+    const folderNavHTML = `
+    <div class="folderNav" style="background-color: white; width: 100%; 
+    margin-top: 47px;">
+        <button id="folder1" style="background-color: #add8e6;">Folder 1</button>
+        <button id="folder2" style="background-color: #add8e6;">Folder 2</button>
+        <button id="folder3" style="background-color: #add8e6;">Folder 3</button>
+        <button id="folder4" style="background-color: #add8e6;">Folder 4</button>
+    </div>
+    `;
+    //if the folder nav is somehow not on the screen already, add it.
+    const folderNav = document.querySelector('.folderNav');
+    if (!folderNav) {
+        document.body.innerHTML += folderNavHTML;
+    }
 
-//#########################################################################################################################################################
-//#########################################################################################################################################################
-//#########################################################################################################################################################
-//#########################################################################################################################################################
+    console.log("Added foldernav.");
+}
+
+
+export function addManageAccountButton() {
+    const navbar = document.querySelector(".navbar");
+    const ManageAccountButton = document.createElement("button");
+    
+    const image = document.createElement("img");
+    image.src = "https://static-00.iconduck.com/assets.00/person-icon-486x512-eeiy7owm.png";
+
+    image.style.width = "25px";
+    image.style.height = "25px";
+
+    // Add the image to the button
+    ManageAccountButton.appendChild(image);
+
+    // Set button properties
+    ManageAccountButton.id = 'ManageAccount';
+    ManageAccountButton.style.width = "25px";
+    ManageAccountButton.style.height = "25px";
+    ManageAccountButton.style.backgroundSize = "contain";
+    ManageAccountButton.style.marginRight = "10px";
+
+
+    // Add the button to the navbar
+    navbar.appendChild(ManageAccountButton);
+
+    console.log("Added ManageAccount button.");
+}
+
 
 /*
 Here, we get the login button and the register button from the popup menu.
@@ -190,5 +359,5 @@ const loginButton = document.querySelector("#loginButton");
 const registerButton = document.querySelector("#registerButton");
 
 
-loginButton.addEventListener("click", login);
+loginButton.addEventListener("click", replaceAccountMenuWithLogin);
 registerButton.addEventListener("click", register);
